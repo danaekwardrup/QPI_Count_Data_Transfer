7b2a9ccb3da38ed573a2a2ef52c0d321dff4a2a5
Danae Wardrup <danaekwardrup@gmail.com>
2021-05-15 13:08:22 -0400

    queried three columns from Recommendations table

--- QCDT_script.py
+++ QCDT_script.py
@@ -1,9 +1,26 @@
 #create the engine
 import pyodbc
-from sqlalchemy import create_engine
-engine = create_engine('mssql+pyodbc://PEUser:peUSER@192.168.100.26:1433/Recommendations_PHFT_YTD?driver=ODBC+Driver+17+for+SQL+Server')
+from sqlalchemy import MetaData, create_engine, Table, Column, Unicode
+from sqlalchemy.sql import select
+metadata = MetaData()
 
+
+
+
+#print out contents of this table Recommendations_P202103_first for testing purposes
+rec = Table('Recommendations_P202103_first', metadata,
+            Column('Organization', Unicode(50)),
+            Column('Patient', Unicode(50)),
+            Column('Recommendation', Unicode(255)))
+
+engine = create_engine('mssql+pyodbc://PEUser:peUSER@192.168.100.106:59647/Recommendations_PHFT_YTD?driver=ODBC+Driver+17+for+SQL+Server')
 #now that engine is initialized, let's open a connection to the database
 connection = engine.connect()
+metadata.create_all(engine)
 
-print(connection)
+s = select([rec])
+rp = connection.execute(s)
+results = rp.fetchall()
+print(results)
+
+
