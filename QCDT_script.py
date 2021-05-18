2598728dd6d33fbb014e58eac4e2199300368d5d
Danae Wardrup <danaekwardrup@gmail.com>
2021-05-18 19:40:34 -0400

    modified connection string so that hitting sbgs so not to interfere with actual run. created for loop to iterate through recommendations and print recs containing certain keyword.

--- QCDT_script.py
+++ QCDT_script.py
@@ -7,20 +7,24 @@ metadata = MetaData()
 
 
 
-#print out contents of this table Recommendations_P202103_first for testing purposes
-rec = Table('Recommendations_P202103_first', metadata,
-            Column('Organization', Unicode(50)),
+#print out contents of recommendations table for testing purposes
+recs = Table('Recommendations_P202009_final', metadata,
+            Column('ProtCode', Unicode(50)),
             Column('Patient', Unicode(50)),
             Column('Recommendation', Unicode(255)))
 
-engine = create_engine('mssql+pyodbc://PEUser:peUSER@192.168.100.106:59647/Recommendations_PHFT_YTD?driver=ODBC+Driver+17+for+SQL+Server')
+engine = create_engine('mssql+pyodbc://PEUser:peUSER@192.168.100.106:59647/Recommendations_SBGS_YTD?driver=ODBC+Driver+17+for+SQL+Server')
 #now that engine is initialized, let's open a connection to the database
 connection = engine.connect()
 metadata.create_all(engine)
 
-s = select([rec])
+s = select([recs])
 rp = connection.execute(s)
-results = rp.fetchall()
-print(results)
+#results = rp.fetchall()
+#print(results)
 
+for i in rp:
+    key = ': current'
+    if key in i[2].lower():
+        print(i[2])
 
