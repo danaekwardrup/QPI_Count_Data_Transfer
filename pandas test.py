01cc987fc10f82c906128f46d4469f356d5e57bb
Danae Wardrup <danaekwardrup@gmail.com>
2021-05-23 11:53:35 -0400

    confirmed .values method is how to iterate through recommendations column. recommendations keywords being counted.

--- pandas test.py
+++ pandas test.py
@@ -3,7 +3,11 @@ import pyodbc
 import pandas as pd
 from sqlalchemy import MetaData, create_engine, Table, Column, Unicode
 from sqlalchemy.sql import select
-
+counter_current = 0
+counter_invalid = 0
+counter_incl = 0
+counter_excl = 0
+counter_excep = 0
 metadata = MetaData()
 
 
@@ -14,6 +18,34 @@ metadata.create_all(engine)
 
 table_df = pd.read_sql_query(
 "SELECT SUBSTRING(Patient, 1, 3) as ptgroup, ProtCode, Recommendation FROM Recommendations_P202009_final",
-con=engine,
+con=engine
 )
-print(table_df)
+
+for rec in table_df[['Recommendation']]:
+    Rec_obj = table_df[rec]
+    rec_array = Rec_obj.values
+
+for rec in rec_array:
+    if ': current' in rec.lower():
+        counter_current += 1
+    if 'excl' in rec.lower():
+        counter_excl += 1
+    if 'incl' in rec.lower():
+        counter_incl += 1
+    if 'invalid' in rec.lower():
+        counter_invalid += 1
+    if 'exception' in rec.lower():
+        counter_excep += 1
+
+print(counter_current)
+print(counter_excl)
+print(counter_incl)
+print(counter_invalid)
+print(counter_excep)
+
+
+
+connection.close()
+
+#grouped_multiple = table_df.groupby(['ptgroup', 'ProtCode'])
+#print(grouped_multiple)
