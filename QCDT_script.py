a2b7d7d889d1ce3a2c1635dfa5ccb3c74bf1ce11
Danae Wardrup <danaekwardrup@gmail.com>
2021-08-29 12:37:27 -0400

    iterate through df's and create list protocols that have no match

--- QCDT_script.py
+++ QCDT_script.py
@@ -88,66 +88,12 @@ for index, table in enumerate(tables):
     totals_df['Performance Rate %'] = totals_df['Met'] / (totals_df['Denominator'] - totals_df['Exception'])
     totals_df['Performance Rate %'] = totals_df['Performance Rate %'].fillna(0)
 
-
-
     if index == 0:
         current_run_recs = totals_df.copy()
         totals_df.drop(['ptgroup', 'ProtCode','Met', 'Not Met','Denominator', 'Exclusion', 'Exception', 'Performance Rate %'],\
         axis=1,inplace=True)
     elif index == 1:
         previous_run_recs = totals_df
-
-"""
-current_run_recs['ProtCode'] = current_run_recs['ProtCode'].astype('string')
-previous_run_recs['ProtCode'] = previous_run_recs['ProtCode'].astype('string')
-current_run_recs['ptgroup'] = current_run_recs['ptgroup'].astype('string')
-previous_run_recs['ptgroup'] = previous_run_recs['ptgroup'].astype('string')
-
-
-prot_names_dict = {}
-for index_a, i in enumerate(current_run_recs['ProtCode']):
-    curr_pg = current_run_recs['ptgroup'][index_a]
-    current_val = i
-    new_prot = True
-    for index_b, x in enumerate(previous_run_recs['ProtCode']):
-        prev_pg = previous_run_recs['ptgroup'][index_b]
-        prev_val = x
-        if curr_pg == prev_pg:
-            if current_val in prev_val or prev_val in current_val:
-                new_prot = False
-                prot_names_dict[current_val] = (prev_val, new_prot)
-
-    if new_prot:
-        print(f"NEW PROT: {current_val}")
-        prot_names_dict[current_val] = ("UNMATCHED", new_prot)
-
-prot_names_list = prot_names_dict.values()
-prev_prot_names_list = []
-
-for i in prot_names_list:
-    prev_prot_names_list.append(i[0])
-
-previous_run_recs = previous_run_recs[previous_run_recs.ProtCode.isin(prev_prot_names_list)]
-
-# create df for difference between 2 tables
-diff_df = current_run_recs[['ptgroup', 'ProtCode']].copy()
-
-for i in current_run_recs['ProtCode']:
-    if current_run_recs['ptgroup'].equals(previous_run_recs['ptgroup']):
-        diff_df['Met']= current_run_recs['Met'] - previous_run_recs['Met']
-        diff_df['Not Met'] = current_run_recs['Not Met'] - previous_run_recs['Not Met']
-        diff_df['Denominator'] = current_run_recs['Denominator'] - previous_run_recs['Denominator']
-        diff_df['Exclusion'] = current_run_recs['Exclusion'] - previous_run_recs['Exclusion']
-        diff_df['Exception'] = current_run_recs['Exception'] - previous_run_recs['Exception']
-        diff_df['Performance Rate %'] = current_run_recs['Performance Rate %'] - previous_run_recs['Performance Rate %']
-"""
-
-
-
-
-
-
-
 
 index_names_current = current_run_recs[(current_run_recs['Denominator'] == 0)\
     & (current_run_recs['Exclusion'] == 0)].index
@@ -162,25 +108,79 @@ previous_run_recs.drop(index_names_prev, inplace=True)
 previous_run_recs.reset_index(inplace=True)
 previous_run_recs.drop(['index'], axis=1, inplace=True)
 
+"""
+current_run_recs.sort_index(inplace=True)
+previous_run_recs.sort_index(inplace=True)
+
+
+for i in current_run_recs['ProtCode']:
+        if current_run_recs['ptgroup'].values.astype(str) != (previous_run_recs['ptgroup'].values.astype(str)):  #TODO:convert to strings
+            blank_rows_to_add.append((current_run_recs['ptgroup'], current_run_recs['ProtCode']))
+        elif current_run_recs['ProtCode'].ne(previous_run_recs['ProtCode']):
+            blank_rows_to_add.append((current_run_recs['ptgroup'], current_run_recs['ProtCode']))
+print(blank_rows_to_add)
+"""
+blank_rows_to_add = []
+missing = 0
+for index, cr_row in current_run_recs.iterrows():
+    cr_ptg = cr_row['ptgroup']
+    cr_pc = cr_row['ProtCode']
+    pr_ptg = None
+    pr_pc = None
+
+    try:
+        pr_ptg = previous_run_recs.iloc[index-missing]['ptgroup']
+        pr_pc = previous_run_recs.iloc[index-missing]['ProtCode']
+    except IndexError:
+        continue
+
+    if cr_ptg != pr_ptg:
+        blank_rows_to_add.append((cr_ptg,cr_pc))
+        missing += 1
+    elif cr_pc != pr_pc:
+        blank_rows_to_add.append((cr_ptg,cr_pc))
+        missing += 1
+
+for row in blank_rows_to_add:
+    print(row)
+
+
+
 
 
 """
 
+for i in current run recs
 
+# create df for difference between 2 tables
+diff_df = current_run_recs[['ptgroup', 'ProtCode']].copy()
+
+for i in current_run_recs['ProtCode']:
+    if current_run_recs['ptgroup'].equals(previous_run_recs['ptgroup']):
+        diff_df['Met']= current_run_recs['Met'] - previous_run_recs['Met']
+        diff_df['Not Met'] = current_run_recs['Not Met'] - previous_run_recs['Not Met']
+        diff_df['Denominator'] = current_run_recs['Denominator'] - previous_run_recs['Denominator']
+        diff_df['Exclusion'] = current_run_recs['Exclusion'] - previous_run_recs['Exclusion']
+        diff_df['Exception'] = current_run_recs['Exception'] - previous_run_recs['Exception']
+        diff_df['Performance Rate %'] = current_run_recs['Performance Rate %'] - previous_run_recs['Performance Rate %']
+
+"""
+
+
+"""
 #change perf rate column to % for all three df's
 
 current_run_recs['Performance Rate %'] = current_run_recs['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
 previous_run_recs['Performance Rate %'] = previous_run_recs['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
 #diff_df['Performance Rate %'] = diff_df['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
 
-"""
 
 #combine the three df's
-combined_df = pd.concat([current_run_recs,previous_run_recs], axis=1)
+combined_df = pd.concat([current_run_recs, previous_run_recs], axis=1)
 
 #insert empty columns
 combined_df.insert(8, '', '', allow_duplicates=True)
-#combined_df.insert(17, '', '', allow_duplicates=True)
+combined_df.insert(17, '', '', allow_duplicates=True)
 
 
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
@@ -189,5 +189,6 @@ file_path = os.path.join(desktop, "run_qa.xlsx")
 
 combined_df.to_excel(file_path, sheet_name='Sheet1')
 
-
+#TODO: HOW TO MATCH PROTS BASED OFF OF PTGROUP, FIRST FEW CHARACTERS OF PROTOCOL...IF NOT MATCH...SPACE
+"""
 connection.close()
