db535ae2d7a0e8487e554595213857d31a0ef477
Danae Wardrup <danaekwardrup@gmail.com>
2021-09-20 14:58:46 -0400

    deleted code to account for prot mismatches. added df to calculate difference.

--- QCDT_script.py
+++ QCDT_script.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import MetaData, create_engine
 import os
-
 metadata = MetaData()
 ptgroup_array = None
 protcode_array = None
@@ -11,10 +10,10 @@ rec_array = None
 
 
 #enter current run rec table, then previous run rec table that you wish to compare
-tables = ["Recommendations_P202104_final", "Recommendations_P202103_final"]
+tables = ["Recommendations_P202105", "Recommendations_P202104"]
 
 engine = create_engine(
-    'mssql+pyodbc://PEUser:peUSER@192.168.100.106:59647/QCDT_HEDIS_YTD_Recs?driver=ODBC+Driver+17+for+SQL+Server')
+    'mssql+pyodbc://PEUser:peUSER@192.168.100.106:59647/Recommendations_NextGen_YTD?driver=ODBC+Driver+17+for+SQL+Server')
 # now that engine is initialized, let's open a connection to the database
 connection = engine.connect()
 metadata.create_all(engine)
@@ -95,6 +94,7 @@ for index, table in enumerate(tables):
     elif index == 1:
         previous_run_recs = totals_df
 
+
 index_names_current = current_run_recs[(current_run_recs['Denominator'] == 0)\
     & (current_run_recs['Exclusion'] == 0)].index
 current_run_recs.drop(index_names_current, inplace=True)
@@ -108,75 +108,25 @@ previous_run_recs.drop(index_names_prev, inplace=True)
 previous_run_recs.reset_index(inplace=True)
 previous_run_recs.drop(['index'], axis=1, inplace=True)
 
-"""
-current_run_recs.sort_index(inplace=True)
-previous_run_recs.sort_index(inplace=True)
-
-
-for i in current_run_recs['ProtCode']:
-        if current_run_recs['ptgroup'].values.astype(str) != (previous_run_recs['ptgroup'].values.astype(str)):  #TODO:convert to strings
-            blank_rows_to_add.append((current_run_recs['ptgroup'], current_run_recs['ProtCode']))
-        elif current_run_recs['ProtCode'].ne(previous_run_recs['ProtCode']):
-            blank_rows_to_add.append((current_run_recs['ptgroup'], current_run_recs['ProtCode']))
-print(blank_rows_to_add)
-"""
-blank_rows_to_add = []
-missing = 0
-for index, cr_row in current_run_recs.iterrows():
-    cr_ptg = cr_row['ptgroup']
-    cr_pc = cr_row['ProtCode']
-    pr_ptg = None
-    pr_pc = None
-
-    try:
-        pr_ptg = previous_run_recs.iloc[index-missing]['ptgroup']
-        pr_pc = previous_run_recs.iloc[index-missing]['ProtCode']
-    except IndexError:
-        continue
-
-    if cr_ptg != pr_ptg:
-        blank_rows_to_add.append((cr_ptg,cr_pc))
-        missing += 1
-    elif cr_pc != pr_pc:
-        blank_rows_to_add.append((cr_ptg,cr_pc))
-        missing += 1
-
-for row in blank_rows_to_add:
-    print(row)
-
-
-
-
-
-"""
-
-for i in current run recs
-
-# create df for difference between 2 tables
+#create difference dataframe
 diff_df = current_run_recs[['ptgroup', 'ProtCode']].copy()
+diff_df['Met'] = current_run_recs['Met'] - previous_run_recs['Met']
+diff_df['Not Met'] = current_run_recs['Not Met'] - previous_run_recs['Not Met']
+diff_df['Denominator'] = current_run_recs['Denominator'] - previous_run_recs['Denominator']
+diff_df['Exclusion'] = current_run_recs['Exclusion'] - previous_run_recs['Exclusion']
+diff_df['Exception'] = current_run_recs['Exception'] - previous_run_recs['Exception']
+diff_df['Performance Rate %'] = current_run_recs['Performance Rate %'] - previous_run_recs['Performance Rate %']
 
-for i in current_run_recs['ProtCode']:
-    if current_run_recs['ptgroup'].equals(previous_run_recs['ptgroup']):
-        diff_df['Met']= current_run_recs['Met'] - previous_run_recs['Met']
-        diff_df['Not Met'] = current_run_recs['Not Met'] - previous_run_recs['Not Met']
-        diff_df['Denominator'] = current_run_recs['Denominator'] - previous_run_recs['Denominator']
-        diff_df['Exclusion'] = current_run_recs['Exclusion'] - previous_run_recs['Exclusion']
-        diff_df['Exception'] = current_run_recs['Exception'] - previous_run_recs['Exception']
-        diff_df['Performance Rate %'] = current_run_recs['Performance Rate %'] - previous_run_recs['Performance Rate %']
-
-"""
-
-
-"""
 #change perf rate column to % for all three df's
 
 current_run_recs['Performance Rate %'] = current_run_recs['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
 previous_run_recs['Performance Rate %'] = previous_run_recs['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
-#diff_df['Performance Rate %'] = diff_df['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
+diff_df['Performance Rate %'] = diff_df['Performance Rate %'].astype(float).map(lambda n: '{:.1%}'.format(n))
+
 
 
 #combine the three df's
-combined_df = pd.concat([current_run_recs, previous_run_recs], axis=1)
+combined_df = pd.concat([current_run_recs, previous_run_recs, diff_df], axis=1)
 
 #insert empty columns
 combined_df.insert(8, '', '', allow_duplicates=True)
@@ -189,6 +139,6 @@ file_path = os.path.join(desktop, "run_qa.xlsx")
 
 combined_df.to_excel(file_path, sheet_name='Sheet1')
 
-#TODO: HOW TO MATCH PROTS BASED OFF OF PTGROUP, FIRST FEW CHARACTERS OF PROTOCOL...IF NOT MATCH...SPACE
-"""
+
+
 connection.close()
